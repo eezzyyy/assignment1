286813159658289471ce12c0329df981f108dc53
eezzyyy <111749926+eezzyyy@users.noreply.github.com>
2022-08-22 18:01:05 +0530

    Add files via upload

---
+++ Alphabets/src/INEURON.java
@@ -0,0 +1,93 @@
+
+public class INEURON {
+
+	public static void main(String[] args) {
+		int n =10 ;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(j == (n-1)/2 || i==0 || i ==n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(j == (n-1) || j==0 || i == j)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(j == 0 || i==0 || i ==(n-1)|| i==(n-1)/2 )
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println(); 
+	}
+		
+		System.out.println();
+		for (int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(j==0 && i!=n-1 || j==n-1 && i!=n-1 ||  i==n-1 && j==(n-1)/2 )
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int i=0; i<n; i++)
+		{
+			for (int j=0; j<n; j++)
+			{
+				if(j==0 || i==0 && j!=n-1 || i==(n-1)/2 && j!=n-1 || j==n-1 && i!=0 && i!=(n-1)/2  )
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	for (int i=0; i<n; i++)
+	{
+		for(int j=0; j<=n; j++)
+		{
+			if( i+j==(n-1)/2 || i-j==(n-1)/2 || i+j==(n-1)+(n-1)/2 || j-i==(n-1)/2)
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+	}
+	System.out.println();
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			if(i==j|| j==0|| j==n-1)
+				System.out.print("*");
+			else
+				System.out.print(" ");
+		}
+		System.out.println();
+		
+	}
+	System.out.println();
+
+
+}
+}
---
+++ Alphabets/src/numbers.java
@@ -0,0 +1,22 @@
+
+public class numbers {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1; j<=n; j++)
+			{
+				if (i==1)
+				System.out.print(i);
+				else 
+					System.out.print(i);
+			}
+			System.out.println();
+			
+		}
+
+
+	}
+
+}
